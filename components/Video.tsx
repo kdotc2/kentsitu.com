ce012a5d9319aa9a8feba17b219cbb759040937a
Kent Situ <kentsitu@icloud.com>
2023-01-03 21:12:30 -0800

    update to video player

--- components/Video.tsx
+++ components/Video.tsx
@@ -20,10 +20,16 @@ const Video = ({ link }) => {
     <div>
       <div className="flex flex-col relative items-center justify-center">
         <div className="absolute z-10">
-          <Image src={`/static/videos/iPhoneXS.png`} width={250} height={503} priority />
+          <Image
+            alt="iPhone screen"
+            src={`/static/videos/iPhoneXS.png`}
+            width={250}
+            height={503}
+            priority
+          />
         </div>
         <div className="">
-          <video className="rounded-[20px]" src={link} ref={videoRef} width={223} loop muted />
+          <video className="rounded-[20px]" src={link} ref={videoRef} width={223} loop muted playsInline/>
         </div>
       </div>
       <div className="flex justify-center font-medium -mt-4 text-blue-500">
