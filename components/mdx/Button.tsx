756f14853090e1cb6611a36c2f2ac6cb99f44f89
Kent <kentsitu@icloud.com>
2025-01-30 12:01:23 -0800

    okay, maybe this time it'll work

---
+++ components/mdx/Button.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Button = ({ link, text }: { link: string; text: string }) => (
+  <div>
+    <Link href={link} target="_blank" rel="noopener noreferrer" className="linkStyle no-underline">
+      <span className="font-medium text-gray-900 dark:text-gray-100">{text}</span>
+    </Link>
+  </div>
+)
+
+export default Button
---
+++ components/mdx/Image.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx'
+import NextImage, { ImageProps } from 'next/image'
+
+const Image = ({ alt, ...rest }: ImageProps) => (
+  <NextImage
+    {...rest}
+    alt={alt}
+    className={clsx(
+      alt.includes('page') &&
+        'rounded-lg border border-gray-200 dark:border-gray-700',
+      (alt.includes('screen') || alt.includes('animation')) &&
+        'border border-gray-200 dark:border-gray-700'
+    )}
+  />
+)
+
+export default Image
