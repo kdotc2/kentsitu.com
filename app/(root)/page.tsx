baacd438c7c8769df0eb5061c30aa0312507e2e2
Kent <kentsitu@icloud.com>
2025-02-27 20:03:37 -0800

    fix padding for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center md:supports-[height:100dvh]:h-[calc(100dvh-144px)] md:m-10 max-md:my-10">
+    <div className="flex flex-col items-center justify-center md:supports-[height:100dvh]:h-[calc(100dvh-144px)] py-10">
       <div className="prose max-w-[450px] dark:prose-invert">
         <p>
           Hello, my name is Kent. Welcome to my space on the web. A place to
